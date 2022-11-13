95cd9112cb7ae3b770c6eee49b7ab084875fcf3e
Peyton Moss <pmoss94@gmail.com>
2022-11-12 23:57:29 -0700

    fixed file names and added header links

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <ul>
+                <li><a href="http://www.youtube.com">Home</a></li>
+                <li>Not Home</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
